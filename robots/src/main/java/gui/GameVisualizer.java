3807a77781b63c9d114287056cb59fc824f69c41
Samareva18 <91691847+Samareva18@users.noreply.github.com>
2024-04-30 05:44:49 +0500

    add background fields

--- robots/src/main/java/gui/GameVisualizer.java
+++ robots/src/main/java/gui/GameVisualizer.java
@@ -6,32 +6,41 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
+import main.java.gui.MazeGenerator;
 import main.java.gui.Maze;
+
+import static main.java.gui.MazeGenerator.generateMaze;
 
 public class GameVisualizer extends JPanel {
 
     private int[][] maze = {                          //TODO вынести в отдельный класс + функция для генерации лабиринта
-            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-            {1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-            {1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1},
-            {1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0},
-            {1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1},
-            {1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1},
-            {1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1},
-            {1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1},
-            {1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1},
-            {1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1},
-            {0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1},
-            {1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1},
-            {1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1},
-            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1},
+            {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+            {1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1},
+            {0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1},
+            {1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1},
+            {1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1},
+            {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1},
+            {0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0},
+            {1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1},
+            {1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1}
     };
 
+    //private int[][] maze = generateMaze(14, 14);
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -47,14 +56,44 @@ public class GameVisualizer extends JPanel {
             for (int j = 0; j < maze[i].length; j++) {
                 if (maze[i][j] == 1) {
                     g.setColor(Color.PINK);
+                    g.fillRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
+
+                    try {
+                        backgroundImage = ImageIO.read(new File("C:\\Users\\user\\git\\Robots\\OOPRobots\\robots\\src\\main\\java\\resources\\grass.png"));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if (backgroundImage != null) {
+                        g.drawImage(backgroundImage, j * cellWidth, i * cellHeight, cellWidth, cellHeight, this);
+                    }
                 } else {
-                    g.setColor(Color.WHITE);
+                    g.setColor(Color.YELLOW);
+                    g.fillRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
+                    try {
+                        backgroundImage = ImageIO.read(new File("C:\\Users\\user\\git\\Robots\\OOPRobots\\robots\\src\\main\\java\\resources\\img.png"));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if (backgroundImage != null) {
+                        g.drawImage(backgroundImage, j * cellWidth, i * cellHeight, cellWidth, cellHeight, this);
+                    }
                 }
-                g.fillRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
+               // g.fillRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
+
+//                try {
+//                    backgroundImage = ImageIO.read(new File("C:\\Users\\user\\git\\Robots\\OOPRobots\\robots\\src\\main\\java\\resources\\grass.png"));
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                if (backgroundImage != null) {
+//                    g.drawImage(backgroundImage, j * cellWidth, i * cellHeight, cellWidth, cellHeight, this);
+//                }
+
             }
         }
 
     }
+
     private final Timer m_timer = initTimer();
 
     private static Timer initTimer() {
@@ -68,15 +107,20 @@ public class GameVisualizer extends JPanel {
 
     int robotSize = 30;
     int halfRobotSize = robotSize / 2;
-//    private Maze maze;
 
-    private float windowHeight = getHeight();
-    private float windowWidth = getWidth();
+    private BufferedImage m_characterImage;
+    private BufferedImage backgroundImage;
+
+
+//    private Maze m_maze;
+
+//    private float windowHeight = getHeight();
+//    private float windowWidth = getWidth();
 
 
     public GameVisualizer() {
-//        maze = new Maze(windowHeight, windowWidth);
-//        add(maze);
+//        m_maze = new Maze(700, 700);
+//        add(m_maze);
         m_timer.schedule(new TimerTask() {
             @Override
             public void run() {
@@ -105,11 +149,11 @@ public class GameVisualizer extends JPanel {
                 switch (keyCode) {
                     case KeyEvent.VK_UP:
                         moveRobot(0, -5);
-                        m_robotDirection = -Math.PI/2;
+                        m_robotDirection = -Math.PI / 2;
                         break;
                     case KeyEvent.VK_DOWN:
                         moveRobot(0, 5);
-                        m_robotDirection = Math.PI/2;
+                        m_robotDirection = Math.PI / 2;
                         break;
                     case KeyEvent.VK_LEFT:
                         moveRobot(-5, 0);
@@ -178,33 +222,10 @@ public class GameVisualizer extends JPanel {
     }
 
 
-//    private void moveRobot(double velocity, double angularVelocity, double duration) {
-//        velocity = applyLimits(velocity, 0, maxVelocity);
-//        angularVelocity = applyLimits(angularVelocity, -maxAngularVelocity, maxAngularVelocity);
-//        double newX = m_robotPositionX + velocity / angularVelocity *
-//                (Math.sin(m_robotDirection + angularVelocity * duration) -
-//                        Math.sin(m_robotDirection));
-//        if (!Double.isFinite(newX)) {
-//            newX = m_robotPositionX + velocity * duration * Math.cos(m_robotDirection);
-//        }
-//        double newY = m_robotPositionY - velocity / angularVelocity *
-//                (Math.cos(m_robotDirection + angularVelocity * duration) -
-//                        Math.cos(m_robotDirection));
-//        if (!Double.isFinite(newY)) {
-//            newY = m_robotPositionY + velocity * duration * Math.sin(m_robotDirection);
-//        }
-//        m_robotPositionX = newX;
-//        m_robotPositionY = newY;
-//        double newDirection = asNormalizedRadians(m_robotDirection + angularVelocity * duration);
-//        m_robotDirection = newDirection;
-//    }
-
-
-    private void moveRobot(double dx, double dy){
+    private void moveRobot(double dx, double dy) {
         m_robotPositionX += dx;
         m_robotPositionY += dy;
     }
-
 
 
     private static int round(double value) {
@@ -228,23 +249,49 @@ public class GameVisualizer extends JPanel {
     }
 
     private void drawRobot(Graphics2D g, double direction) {
-        int robotCenterX = round(m_robotPositionX);
-        int robotCenterY = round(m_robotPositionY);
+//        int robotCenterX = round(m_robotPositionX);
+//        int robotCenterY = round(m_robotPositionY);
+//
+//        AffineTransform t = AffineTransform.getRotateInstance(direction, robotCenterX, robotCenterY);
+//        g.setTransform(t);
+//        g.setColor(Color.MAGENTA);
+//        fillOval(g, robotCenterX, robotCenterY, 30, 10);
+//        g.setColor(Color.BLACK);
+//        drawOval(g, robotCenterX, robotCenterY, 30, 10);
+//        g.setColor(Color.WHITE);
+//        fillOval(g, robotCenterX + 10, robotCenterY, 5, 5);
+//        g.setColor(Color.BLACK);
+//        drawOval(g, robotCenterX + 10, robotCenterY, 5, 5);
 
-        AffineTransform t = AffineTransform.getRotateInstance(direction, robotCenterX, robotCenterY);
+        loadCharacterImage();
+        int characterCenterX = round(m_robotPositionX);
+        int characterCenterY = round(m_robotPositionY);
+
+        AffineTransform t = AffineTransform.getRotateInstance(direction, characterCenterX, characterCenterY);
         g.setTransform(t);
-        g.setColor(Color.MAGENTA);
-        fillOval(g, robotCenterX, robotCenterY, 30, 10);
-        g.setColor(Color.BLACK);
-        drawOval(g, robotCenterX, robotCenterY, 30, 10);
-        g.setColor(Color.WHITE);
-        fillOval(g, robotCenterX + 10, robotCenterY, 5, 5);
-        g.setColor(Color.BLACK);
-        drawOval(g, robotCenterX + 10, robotCenterY, 5, 5);
 
+        if (m_characterImage != null) {
+            g.drawImage(m_characterImage, characterCenterX - m_characterImage.getWidth(null) / 2,
+                    characterCenterY - m_characterImage.getHeight(null) / 2, null);
+
+        }
+//        if (characterImage != null) {
+//            g.drawImage(characterImage, characterCenterX - characterImage.getWidth(null) / 2,
+//                    characterCenterY - characterImage.getHeight(null) / 2, null);
+//
+//        }
     }
 
-    private void drawRobotInBounds(Graphics2D g, double direction){
+    private void loadCharacterImage() {
+        try {
+            m_characterImage = ImageIO.read(new File("C:\\Users\\user\\git\\Robots\\OOPRobots\\robots\\src\\main\\java\\resources\\rabbit1.png"));
+           // characterImage = ImageIO.read(new File("C:\\Users\\user\\git\\Robots\\OOPRobots\\robots\\src\\main\\java\\resources\\bird.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void drawRobotInBounds(Graphics2D g, double direction) {
         int robotCenterX = round(m_robotPositionX);
         int robotCenterY = round(m_robotPositionY);
 
@@ -270,7 +317,7 @@ public class GameVisualizer extends JPanel {
         }
     }
 
-    private void drawRobotWithinPath(Graphics2D g, double direction){
+    private void drawRobotWithinPath(Graphics2D g, double direction) {
 
         int robotCenterX = round(m_robotPositionX);
         int robotCenterY = round(m_robotPositionY);
@@ -284,11 +331,11 @@ public class GameVisualizer extends JPanel {
         int cellWidth = round(windowWidth / countOfCellsInWidth);
         int cellHeight = round(windowHeight / countOfCellsInHeight);
 
-        int i1 = robotCenterY  / cellHeight;
-        int j1 = (robotCenterX + halfRobotSize)/ cellWidth;
+        int i1 = robotCenterY / cellHeight;
+        int j1 = (robotCenterX + halfRobotSize) / cellWidth;
 
-        int i2 = robotCenterY  / cellHeight;
-        int j2 = (robotCenterX - halfRobotSize)/ cellWidth;
+        int i2 = robotCenterY / cellHeight;
+        int j2 = (robotCenterX - halfRobotSize) / cellWidth;
 
         int i3 = (robotCenterY - halfRobotSize) / cellHeight;
         int j3 = robotCenterX / cellWidth;
@@ -303,15 +350,14 @@ public class GameVisualizer extends JPanel {
         if (maze[i2][j2] == 1 && direction == Math.PI) { //влево
             moveRobot(5, 0);
         }
-        if (maze[i3][j3] == 1 && direction == -Math.PI/2) { //вверх
+        if (maze[i3][j3] == 1 && direction == -Math.PI / 2) { //вверх
             moveRobot(0, 5);
         }
-        if (maze[i4][j4] == 1 && direction == Math.PI/2) { //вниз
+        if (maze[i4][j4] == 1 && direction == Math.PI / 2) { //вниз
             moveRobot(0, -5);
         }
 
     }
-
 
 
 //    private void drawTarget(Graphics2D g, int x, int y) {
